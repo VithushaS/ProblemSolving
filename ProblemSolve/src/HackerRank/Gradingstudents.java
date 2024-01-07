80371898d38aa24ef46d1f3fb58a1edc447bfff9
VithushaS <vithuselva1502@gmail.com>
2024-01-07 13:30:40 +0530

    2nd commit

--- ProblemSolve/src/HackerRank/Gradingstudents.java
+++ ProblemSolve/src/HackerRank/Gradingstudents.java
@@ -1,4 +1,12 @@
 package HackerRank;
+  /*
+ * Every student receives a  in the inclusive range from  0 to 100.
+Any grade  less than 40 is a failing grade. Sam is a professor at the university and likes
+ to round each student's  grade according to these rules:
+  If the difference between the  grade and the next multiple of  is 5 less than 3 ,
+  round grade up to the next multiple of 5. If the value of  grade is less than 38 ,
+   no rounding occurs as the result will still be a failing grade.
+ */
 
 import java.util.Scanner;
 
